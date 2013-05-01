cdee333a66666365acab7701f970d684a2864cb1
Dimitrios Paraschas <paraschas@gmail.com>
2013-05-01 11:08:10 +0300

    New function templates. Some more changes.
    The function templates for process manipulation are: process_exec,
    process_kill, process_stop, process_cont, process_list, process_info
    
    New function template for reading user input: get_input
    
    The reference to ncurses was removed, I think it is premature to use it.

--- a3.c
+++ a3.c
@@ -13,12 +13,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-/*#include <curses.h>*/
+#include <stdlib.h>
 ////////////////////////////////////////////////////////////////////////////////
 
 // #define directives
 ////////////////////////////////////////////////////////////////////////////////
 #define MAX_TASK_LENGTH 4
+#define MAX_INPUT_LENGTH 1024
 ////////////////////////////////////////////////////////////////////////////////
 
 // custom data types
@@ -48,11 +49,41 @@ void clear_screen() {
     // clear_screen does not return any value.
 
     // variable declaration
-    //const char *cls_ansi_string = "\e[1;1H\e[2J";
+    //const char cls_ansi_string[] = "\e[1;1H\e[2J";  // TODO The pointer asterisk
+            // was replaced by square brackets. Verify their equivalence.
 
     //printf("%s", cls_ansi_string);
 
     printf("\e[1;1H\e[2J");
+}
+
+int get_input(int *num_elements, char *elements[]) {
+
+    return 0;
+}
+
+void process_exec(char *progname, (char *)NULL) {
+    /*return_value = execv*/
+}
+
+int process_kill() {
+    return 0;
+}
+
+int process_stop() {
+    return 0;
+}
+
+int process_cont() {
+    return 0;
+}
+
+int process_list() {
+    return 0;
+}
+
+int process_info() {
+    return 0;
 }
 
 int task_queue() {
@@ -64,56 +95,49 @@ int task_queue() {
 
     // variable declaration
     char task[MAX_TASK_LENGTH + 1];  // task code
+    int num_elements;
+    char *elements[];
+    int return_value;
+    /*char *return_value;  // placeholder pointer for error checking*/
+    int return_value;  // integer placeholder for error checking
 
     // Clear the screen.
-    // TODO Maybe implement with ncurses.
     /*clear_screen();*/
 
     // Print a welcome message.
     printf("\tSCEE signal controlled execution environment\n\n");
 
+    // TODO Print the program functions.
+
     strcpy(task, "");
     while (strcmp(task, "quit")) {
-        return_value = get_task(task);
+        return_value = get_input(num_elements, elements);
         if (return_value == -1) {
             // TODO Research error checking with macros.
             perror("get_task error");
         }
 
-        if (!strcmp(task, "exec")) {
+        if (!strcmp(task, "exec") || !strcmp(task, "e")) {
             process_exec();
-        } else if (!strcmp(task, "kill")) {
+        } else if (!strcmp(task, "kill") || !strcmp(task, "k")) {
             process_kill();
-        } else if (!strcmp(task, "stop")) {
+        } else if (!strcmp(task, "stop") || !strcmp(task, "s")) {
             process_stop();
-        } else if (!strcmp(task, "cont")) {
+        } else if (!strcmp(task, "cont") || !strcmp(task, "c")) {
             process_cont();
-        } else if (!strcmp(task, "list")) {
+        } else if (!strcmp(task, "list") || !strcmp(task, "l")) {
             process_list();
-        } else if (!strcmp(task, "info")) {
+        } else if (!strcmp(task, "info") || !strcmp(task, "i")) {
+            process_info();
+        } else if (!strcmp(task, "quit") || !strcmp(task, "q")) {
             process_info();
         } else {
-            printf("No valid task requested.");
+                printf("No valid task requested.");
         }
     }
 
     return 0;
 }
-
-void process_exec(char *progname, (char *)NULL) {
-    // TODO or execv
-    execl
-}
-
-process_kill();
-
-process_stop();
-
-process_cont();
-
-process_list();
-
-process_info();
 
 ////////////////////////////////////////////////////////////////////////////////
 
